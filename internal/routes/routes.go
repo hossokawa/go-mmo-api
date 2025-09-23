866976d2dd22799a3346aa4cfb22c4da4f14d897
hossokawa <leonardoholiver@gmail.com>
2025-09-22 21:03:09 -0300

    style: move POST route to first in list of routes

--- internal/routes/routes.go
+++ internal/routes/routes.go
@@ -13,7 +13,7 @@ func SetupRoutes(router *http.ServeMux, db *pgx.Conn) {
 	playerService := player.NewPlayerService(playerRepo)
 	playerHandler := handler.NewPlayerHandler(playerService)
 
+	router.HandleFunc("POST /player", playerHandler.CreatePlayer)
 	router.HandleFunc("GET /player", playerHandler.GetAllPlayers)
 	router.HandleFunc("GET /player/{id}", playerHandler.GetPlayerByID)
-	router.HandleFunc("POST /player", playerHandler.CreatePlayer)
 }
